0c7b933e4bb452774639d48d17c262842ea58387
Burak <burakkiran95@gmail.com>
2019-02-19 17:34:21 -0800

    first commit

---
+++ dataBaseOpps.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+//Thes operations are for a table that has the following structure.
+//Make changes according to table structure.
+// TABLE users (
+// 	id SERIAL PRIMARY KEY,
+// 	age INT,
+// 	first_name TEXT,
+// 	last_name TEXT,
+// 	email TEXT UNIQUE NOT NULL
+// )
+
+//Person object to represent table structure
+type person struct {
+	ID        int    `json:"id"`
+	Age       int    `json:"age"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+}
+
+//QueryAllUsers retrives all users in DB
+func QueryAllUsers(db *sql.DB) ([]person, error) {
+	rows, err := db.Query("SELECT id, age, first_name, last_name, email FROM users")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	persons := []person{}
+
+	for rows.Next() {
+		var p person
+		err = rows.Scan(&p.ID, &p.Age, &p.FirstName, &p.LastName, &p.Email)
+		if err != nil {
+			return nil, err
+		}
+		persons = append(persons, p)
+	}
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return persons, err
+}
+
+//QueryUser querys 1 user
+func QueryUser(db *sql.DB, id int) (person, error) {
+	sqlStatement := `SELECT id, age, first_name, last_name, email FROM users WHERE id=$1;`
+	var p person
+	row := db.QueryRow(sqlStatement, id)
+	err := row.Scan(&p.ID, &p.Age, &p.FirstName, &p.LastName, &p.Email)
+	switch err {
+	case sql.ErrNoRows:
+		return p, err
+	case nil:
+		//fmt.Println(user)
+		return p, err
+	default:
+		return p, err
+	}
+}
+
+//CreateNewUser Insert user into table
+func CreateNewUser(db *sql.DB, age int, email string, firstName string, lastName string) (int, error) {
+	//Insert user in table
+	sqlStatement := `
+	INSERT INTO users (age, email, first_name, last_name)
+	VALUES ($1, $2, $3, $4)
+	RETURNING id`
+	var id int
+	err := db.QueryRow(sqlStatement, age, email, firstName, lastName).Scan(&id)
+	if err != nil {
+		return id, err
+	}
+	return id, err
+}
+
+//UpdateUser edits a single user
+func UpdateUser(db *sql.DB) {
+	//Update user in table
+	sqlStatement := `
+	UPDATE users
+	SET first_name = $2, last_name = $3
+	WHERE id = $1;`
+	res, err := db.Exec(sqlStatement, 5, "NewFIrst", "NewLast")
+	if err != nil {
+		panic(err)
+	}
+	count, err := res.RowsAffected()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(count)
+
+}
+
+//DeleteUser deletes a user from table
+func DeleteUser(db *sql.DB, id int) string {
+	//Delete user in table
+	sqlStatement := `
+	DELETE users
+	WHERE id=$1;`
+	_, err := db.Exec(sqlStatement, id)
+	if err != nil {
+		return "User cannot be deleted, invalid id."
+	}
+
+	return "User with id deleted"
+}
---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	_ "github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+//Database credentials, used to connect to db
+const (
+	host     = "host_name"
+	port     = 1111
+	user     = "postgres"
+	password = "password"
+	dbname   = "name_of_database"
+)
+
+//gloabl variable so that db can be used accross other files
+var db *sql.DB
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	//fmt.Println("Request Recived")
+	persons, err := QueryAllUsers(db)
+	if err != nil {
+		return
+	}
+	response, _ := json.Marshal(persons)
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Request Recived")
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		fmt.Println("Invalid user id")
+		return
+	}
+
+	p, err := QueryUser(db, id)
+	if err != nil {
+		fmt.Println("User does not exist")
+	}
+	response, _ := json.Marshal(p)
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
+func createNewUser(w http.ResponseWriter, r *http.Request) {
+	var p person
+	fmt.Println("Create user called.")
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&p)
+	if err != nil {
+		fmt.Println("Invalid POST request")
+		return
+	}
+	id, err := CreateNewUser(db, p.Age, p.FirstName, p.LastName, p.Email)
+	if err != nil {
+		panic(err)
+	}
+	// defer r.Body.Close()
+	fmt.Println(id)
+
+	response, _ := json.Marshal(p)
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Update user called.")
+}
+
+func main() {
+	var err error
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	//fmt.Println("Conected to database successfully.")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/users", getUsers).Methods("GET")
+	r.HandleFunc("/user/{id:[0-9]+}", getUser).Methods("GET")
+	r.HandleFunc("/user", createNewUser).Methods("POST")
+
+	log.Fatal(http.ListenAndServe("localhost:8000", r))
+
+}
